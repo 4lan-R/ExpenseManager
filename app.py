9498993a3e182ece6d62df708e4ad5c655ea5608
Alan <alanremanan@gmail.com>
2026-05-19 16:21:58 +0530

    Fix expense retrieval logic in get_expense_by_month function and implement total expense calculation

--- app.py
+++ app.py
@@ -36,10 +36,24 @@ def get_all_expenses():
     return {"expenses": expenses}
 
 @app.get("/expenses_by_month/{month}")
-def get_all_expenses():
+def get_expense_by_month(month: str):
+    con = sqlite3.connect("Expense.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM expense WHERE name='{month}'")
+    rows = cur.fetchall()
+    expenses = [Expense(name=row[1], amount=row[2], category=row[3], month=row[4], year=row[5]) for row in rows]
+    return {"expenses": expenses}
+
+
+@app.get("/total_expense")
+def get_expense_by_month():
     con = sqlite3.connect("Expense.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM expense")
     rows = cur.fetchall()
-    expenses = [Expense(name=row[1], amount=row[2], category=row[3]) for row in rows]
-    return {"expenses": expenses}
+    expenses = [Expense(name=row[1], amount=row[2], category=row[3], month=row[4], year=row[5]) for row in rows]
+
+    for i in expenses[2]:
+        total= total+i
+
+    return {"total expenses": total}
